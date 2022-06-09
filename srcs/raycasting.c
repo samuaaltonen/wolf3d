d0742e8c2dd51e2883648aaf920f5479bb6e1f12
Samu Aaltonen <saaltone@student.hive.fi>
2022-06-09 14:43:39 +0300

    Cleaned up, shortened and simplified function get_wall_distance.

--- srcs/raycasting.c
+++ srcs/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 13:14:55 by saaltone          #+#    #+#             */
-/*   Updated: 2022/06/09 13:20:22 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/06/09 14:43:05 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,43 +34,33 @@ static t_vector2d	get_side_distance(t_app *app, t_vector2d *pos,
 /*
  * Calculates wall distance using DDA method (Digital differential analyzer). 
 */
-static double	get_wall_distance(t_app *app, t_vector2d *pos, t_vector2d *ray)
+static double	get_wall_distance(t_app *app, t_vector2d *pos,
+	t_vector2d *ray, t_vector2d *delta_dist)
 {
 	t_vector2d	side_dist;
-	t_vector2d	delta_dist;
-	int			side;
 
-	delta_dist = (t_vector2d){fabs(1.f / ray->x), fabs(1.f / ray->y)};
-	side_dist = get_side_distance(app, pos, ray, &delta_dist);
-	// Start DDA
+	side_dist = get_side_distance(app, pos, ray, delta_dist);
 	while (1)
 	{
-		// Next map box (if x is closer go to x side...)
 		if (side_dist.x < side_dist.y)
 		{
-			side_dist.x += delta_dist.x;
+			side_dist.x += delta_dist->x;
 			if (ray->x < 0)
 				pos->x += -1.f;
 			else
 				pos->x += 1.f;
-			side = 0;
+			if (app->map[(int) pos->y][(int) pos->x])
+				return (side_dist.x - delta_dist->x);
+			continue ;
 		}
+		side_dist.y += delta_dist->y;
+		if (ray->y < 0)
+			pos->y += -1.f;
 		else
-		{
-			side_dist.y += delta_dist.y;
-			if (ray->y < 0)
-				pos->y += -1.f;
-			else
-				pos->y += 1.f;
-			side = 1;
-		}
-		// Check hit
+			pos->y += 1.f;
 		if (app->map[(int) pos->y][(int) pos->x])
-			break;
+			return (side_dist.y - delta_dist->y);
 	}
-	if (!side)
-		return (side_dist.x - delta_dist.x);
-	return (side_dist.y - delta_dist.y);	
 }
 
 /*
@@ -81,11 +71,13 @@ int	raycast(t_app *app, int x, double *distance)
 	double		camera_x;
 	t_vector2d	ray;
 	t_vector2d	pos;
+	t_vector2d	delta_dist;
 
 	camera_x = 2 * x / (double) WIN_W - 1.f;
 	ray.x = app->player.direction.x + app->player.camera_plane.x * camera_x;
 	ray.y = app->player.direction.y + app->player.camera_plane.y * camera_x;
 	pos = (t_vector2d){app->player.position.x, app->player.position.y};
-	*distance = get_wall_distance(app, &pos, &ray);
+	delta_dist = (t_vector2d){fabs(1.f / ray.x), fabs(1.f / ray.y)};
+	*distance = get_wall_distance(app, &pos, &ray, &delta_dist);
 	return (app->map[(int) pos.y][(int) pos.x]);
 }
