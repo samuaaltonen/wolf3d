4e92d9a763652fdf4031bdc0525ffad58ee3574b
Samu Aaltonen <saaltone@student.hive.fi>
2022-07-20 15:56:47 +0300

    Fixed an issue where doors were only added on object blocks.

---
+++ srcs/door.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   door.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 14:11:42 by saaltone          #+#    #+#             */
+/*   Updated: 2022/07/20 15:55:20 by saaltone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+
+/**
+ * Calculates door count in the map.
+*/
+static int	get_door_count(t_app *app)
+{
+	int	x;
+	int	y;
+	int	count;
+
+	y = 0;
+	count = 0;
+	while (y < app->map_size.y)
+	{
+		x = 0;
+		while (x < app->map_size.x)
+		{
+			if (app->map[y][x][0] == DOOR_MAP_IDENTIFIER)
+				count++;
+			x++;
+		}
+		y++;
+	}
+	return (count);
+}
+
+/**
+ * Reads all doors from the map and initilizes an array containing positions
+ * of the doors and initial states.
+*/
+void	init_doors(t_app *app)
+{
+	int	x;
+	int	y;
+	int	i;
+
+	app->doors = (t_door*)malloc(sizeof(t_door) * (get_door_count(app) + 1));
+	if (!app->doors)
+		exit_error(MSG_ERROR_ALLOC);
+	y = -1;
+	i = 0;
+	while (++y < app->map_size.y)
+	{
+		x = -1;
+		while (++x < app->map_size.x)
+		{
+			if (app->map[y][x][0] == DOOR_MAP_IDENTIFIER)
+			{
+				app->doors[i] = (t_door){
+					(t_point){x, y},
+					CLOSED,
+					0.f
+				};
+				i++;
+			}
+		}
+	}
+	app->door_count = i;
+}
+
+/**
+ * Handles door action and opens or closes it depending on the door state.
+*/
+void	door_action(t_app *app)
+{
+	(void)app;
+}
