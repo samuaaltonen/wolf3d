ee266fc9f667203368c9b67e23a554d4f2cf6dcc
Samu Aaltonen <saaltone@student.hive.fi>
2022-08-08 17:05:48 +0300

    Simplified main: removed unnecessary fd and opening of the map file.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 14:21:47 by saaltone          #+#    #+#             */
-/*   Updated: 2022/08/08 16:15:04 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/08/08 17:05:08 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,22 @@ int	main(int argc, char **argv)
 {
 	t_app	*app;
 	char	*path;
-	int		fd;
 
-	fd = -1;
-	app = NULL;
+	if (argc > 2)
+		exit_error(MSG_ERROR_TOO_MANY_ARGUMENTS);
 	if (argc == 2)
-	{
-		path = ft_strnew(ft_strlen(argv[1]));
-		if (!path)
-			exit_error(NULL);
-		ft_strcpy(path, argv[1]);
-		fd = open(path, O_RDONLY);
-		if (fd < 0 || !app_init(&app, path) || !parse_map(app, path)
-			||!conf_init(app))
-			exit_error(NULL);
-	}
-	else if (!app_init(&app, MAP_FILE) || !parse_map(app, MAP_FILE)
+		path = ft_strdup(argv[1]);
+	else
+		path = ft_strdup(MAP_FILE_DEFAULT);
+	if (!path)
+		exit_error(NULL);
+	app = NULL;
+	if (!path
+		|| !app_init(&app, path)
+		|| !parse_map(app, path)
 		|| !conf_init(app))
 		exit_error(NULL);
+	free(path);
 	app_prepare(app);
 	mlx_loop(app->mlx);
 	return (0);
